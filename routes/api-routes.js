c073386ca875ae0e76d33262721fcd4316c35160
ttran1185 <ttran1185@gmail.com>
2020-09-01 20:20:13 -0700

    update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 // connection configurations
 const connection = mysql.createConnection({
-    host: 'ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'bm9fown58etjzehn',
-    password: 'v6xvnr9rdq2kal2c',
-    database: 'byq0vips69aj56gv'
+    host: 'tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 're2n4z84gpws0cbn',
+    password: 'iupdavy6n14u6wfc',
+    database: 'r9lh7uwbposuvlg6'
 });
 
 // connect to database
